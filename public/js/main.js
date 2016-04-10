c998d8e9a697f64ffa83cdfe51312fd0e282943c
editor <zdy23216340@163.com>
2016-04-10 13:19:48 +0800

    修改nav

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 var show_li = function (par, el) {
     el = par.childNodes[1];
     if (el.style.display === 'block') {
+        par.childNodes[0].nodeValue = par.childNodes[0].nodeValue.replace('+', '-')
         return el.style.display = 'none';
     }
+    par.childNodes[0].nodeValue = par.childNodes[0].nodeValue.replace('-', '+')
     el.style.display = 'block';
 }
 
@@ -14,7 +16,8 @@ window.onload = function (a, b, i) {
             b.style.background = '#5db564';
             b.style.color = '#fff';
             b.parentNode.style.display = 'block';
-            b.childNodes[1].innerHTML = '┗';
+            b.childNodes[1].innerHTML = '+';
+            b.parentNode.previousSibling.nodeValue = b.parentNode.previousSibling.nodeValue.replace('-', '+')
         }
     }
 }
